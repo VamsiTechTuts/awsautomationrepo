53645988f0ab55e719eb2452e32c169de51b52e1
VamsiTechTuts <63221837+VamsiTechTuts@users.noreply.github.com>
2020-05-18 21:22:07 +0530

    Update install_docker_machine_compose.sh

--- deploy-docker-swarm-using-terraform-ansible/install_docker_machine_compose.sh
+++ deploy-docker-swarm-using-terraform-ansible/install_docker_machine_compose.sh
@@ -15,10 +15,10 @@ sudo echo -e "ansible ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
 
 sudo sed -i '/PasswordAuthentication no/c\PasswordAuthentication yes' /etc/ssh/sshd_config
 sudo service sshd restart
-sudo mkdir -p /home/ansible
-sudo mkdir -p /home/ansible/.ssh
+sudo mkdir /home/ansible
+sudo mkdir /home/ansible/.ssh
 sudo touch /home/ansible/.ssh/authorized_keys
-sudo chown -R ansible:ansible ansible
+sudo chown -R ansible:ansible /home/ansible
 
 # install docker-engine
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
