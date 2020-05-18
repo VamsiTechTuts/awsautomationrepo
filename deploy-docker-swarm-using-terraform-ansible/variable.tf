f635fce6c9a189b90113d25e70ffa2bd4a307f5b
root <root@ip-172-31-17-192.ec2.internal>
2020-05-18 11:55:04 +0000

    deploy-docker-swarm-using-terraform-ansible

---
+++ deploy-docker-swarm-using-terraform-ansible/variable.tf
@@ -0,0 +1,29 @@
+variable "aws_region" {
+  description = "AWS region on which we will setup the swarm cluster"
+  default = "us-east-1"
+}
+
+variable "ami" {
+  description = "Amazon Linux AMI"
+  default = "ami-085925f297f89fce1"
+}
+
+variable "instance_type" {
+  description = "Instance type"
+  default = "t2.micro"
+}
+
+variable "key_path" {
+  description = "SSH Public Key path"
+  default = "/root/May.pem"
+}
+
+variable "key_name" {
+  description = "Desired name of Keypair..."
+  default = "May"
+}
+
+variable "bootstrap_path" {
+  description = "Script to install Docker Engine"
+  default = "install_docker_machine_compose.sh"
+}
