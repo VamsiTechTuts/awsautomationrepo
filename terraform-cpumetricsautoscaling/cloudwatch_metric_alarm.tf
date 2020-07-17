ca4a8fc476102f03a2b29a2cfd8d5fc59101536c
VamsiTechTuts <63221837+VamsiTechTuts@users.noreply.github.com>
2020-07-17 18:11:43 +0530

    Update cloudwatch_metric_alarm.tf

--- terraform-cpumetricsautoscaling/cloudwatch_metric_alarm.tf
+++ terraform-cpumetricsautoscaling/cloudwatch_metric_alarm.tf
@@ -1,4 +1,4 @@
-resource "aws_cloudwatch_metric_alarm" "memory-high" {
+resource "aws_cloudwatch_metric_alarm" "cpu-high" {
     alarm_name = "cpu-utilization-high"
     comparison_operator = "GreaterThanOrEqualToThreshold"
     evaluation_periods = "2"
@@ -7,7 +7,7 @@ resource "aws_cloudwatch_metric_alarm" "memory-high" {
     period = "60"
     statistic = "Average"
     threshold = "40"
-    alarm_description = "This metric monitors ec2 memory for high utilization on agent hosts"
+    alarm_description = "This metric monitors ec2 cpu for high utilization on agent hosts"
     alarm_actions = [
         aws_autoscaling_policy.agents-scale-up.arn
     ]
@@ -16,7 +16,7 @@ resource "aws_cloudwatch_metric_alarm" "memory-high" {
     }
 }
 
-resource "aws_cloudwatch_metric_alarm" "memory-low" {
+resource "aws_cloudwatch_metric_alarm" "cpu-low" {
     alarm_name = "cpu-utilization-low"
     comparison_operator = "LessThanOrEqualToThreshold"
     evaluation_periods = "2"
@@ -25,7 +25,7 @@ resource "aws_cloudwatch_metric_alarm" "memory-low" {
     period = "60"
     statistic = "Average"
     threshold = "20"
-    alarm_description = "This metric monitors ec2 memory for low utilization on agent hosts"
+    alarm_description = "This metric monitors ec2 cpu for low utilization on agent hosts"
     alarm_actions = [
         aws_autoscaling_policy.agents-scale-down.arn
     ]
