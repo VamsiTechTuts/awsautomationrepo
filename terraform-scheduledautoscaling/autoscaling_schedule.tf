f38ba9e8fc3ca5812a595bcaf6ff4c54daf4027b
root <root@ip-172-31-61-54.ec2.internal>
2020-07-17 12:19:34 +0000

    terraform-scheduledautoscaling

--- terraform-scheduledautoscaling/autoscaling_schedule.tf
+++ terraform-scheduledautoscaling/autoscaling_schedule.tf
@@ -1,22 +1,22 @@
-resource "aws_autoscaling_schedule" "start_cron_example" {
+resource "aws_autoscaling_schedule" "scaleup_cron" {
   scheduled_action_name  = "start_cron_example"
   min_size               = 2
   max_size               = 4
-  desired_capacity       = 2
-  start_time             = "2020-07-17T11:10:00Z"
-  end_time               = "2020-07-18T11:25:00Z"
-  recurrence             = "10 11 * * *"
+  desired_capacity       = 4
+  start_time             = var.scaleup_start_time
+  end_time               = var.scaleup_end_time
+  recurrence             = var.scaleup_recurrence
   autoscaling_group_name = aws_autoscaling_group.agents.name
 }
 
-resource "aws_autoscaling_schedule" "stop_cron_example" {
+resource "aws_autoscaling_schedule" "scaledown_cron" {
   scheduled_action_name  = "stop_cron_example"
   min_size               = 2
   max_size               = 4
   desired_capacity       = 2
-  start_time             = "2020-07-17T11:26:00Z"
-  end_time               = "2020-07-18T11:35:00Z"
-  recurrence             = "26 11 * * *"
+  start_time             = var.scaledown_start_time
+  end_time               = var.scaledown_end_time
+  recurrence             = var.scaledown_recurrence
   autoscaling_group_name = aws_autoscaling_group.agents.name
 }
 
--- terraform-scheduledautoscaling/variables.tf
+++ terraform-scheduledautoscaling/variables.tf
@@ -13,4 +13,33 @@ variable "key_name" {
   default = "helloworld"
 }
 
+variable "scaleup_start_time" {
+  description = "start time for scheduling autoscaling"
+  default = "2020-07-17T12:10:00Z"
+}
+
+variable "scaleup_end_time" {
+  description = "end time for scheduling autoscaling"
+  default = "2020-07-18T12:15:00Z"
+}
+
+variable "scaleup_recurrence" {
+  description = "recurrence for scheduling autoscaling"
+  default = "10 12 * * *"
+}
+
+variable "scaledown_start_time" {
+  description = "start time for scheduling autoscaling"
+  default = "2020-07-17T12:10:00Z"
+}
+
+variable "scaledown_end_time" {
+  description = "end time for scheduling autoscaling"
+  default = "2020-07-18T12:15:00Z"
+}
+
+variable "scaledown_recurrence" {
+  description = "recurrence for scheduling autoscaling"
+  default = "10 12 * * *"
+}
 
